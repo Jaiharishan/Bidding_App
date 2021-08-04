f5d6294dd33d5d151f6c8c21663a9a3610933078
Jaiharishan <downdkar@gmail.com>
2021-08-04 22:39:31 +0530

    bidding added

--- routes/app.js
+++ routes/app.js
@@ -20,4 +20,37 @@ router.get('/dashboard', (req, res) => {
 })
 
 
+
+// POST router to get bids and bidders
+router.post('/', (req, res) => {
+    const {bidAmount, itemname, username} = req.body;
+    console.log(bidAmount, itemname);
+    Bid.findOneAndUpdate({bidname: itemname},
+        {
+            bidprice: bidAmount,
+            $push: {
+                bidders: {username: username, bidAmount: bidAmount}
+            }
+        })
+        .then(bid => {
+            if(bid) {
+                console.log(bid);
+            }
+            
+            Bid.find({}, (err, bids) => {
+                if (err) {
+                    res.status('404').send('something went wrong');
+                }
+
+                info.bids = bids;
+
+                res.render('app', info);
+
+            })
+
+        })
+        .catch(err => console.log(err))
+        
+
+})
 module.exports = router;
